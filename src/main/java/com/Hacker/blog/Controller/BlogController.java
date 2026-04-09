c5bf7f2b0114fce16741d7eed13dcba8dbb736a4
Kashish <kashishverma8382@gmail.com>
2026-04-09 21:49:32 +0530

    add basic scraper

--- src/main/java/com/Hacker/blog/Controller/BlogController.java
+++ src/main/java/com/Hacker/blog/Controller/BlogController.java
@@ -1,4 +1,29 @@
 package com.Hacker.blog.Controller;
 
+import com.Hacker.blog.Scraper.Scrape;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/blog")
 public class BlogController {
+
+    @GetMapping("/titles")
+    String getBlogTitles() throws IOException {
+        // Logic to scrape blog titles and return them as a list
+        Scrape scrape = new Scrape();
+        List<Scrape.Post> posts = scrape.scrapeHackerNews();
+        posts.forEach(System.out::println);
+        StringBuilder titles = new StringBuilder();
+        posts.forEach(System.out::println);
+        for(int i=0; i<posts.size(); i++){
+            titles.append(i+1).append(". ").append(posts.get(i).getTitle()).append("\n")
+                    .append(posts.get(i).getUrl()).append("\n");
+        }
+       return titles.toString();
+    }
 }
--- src/main/java/com/Hacker/blog/Scraper/Scrape.java
+++ src/main/java/com/Hacker/blog/Scraper/Scrape.java
@@ -10,16 +10,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Scrape {
-    public List<Scrapes> Scrape() throws IOException {
-        List<Scrapes> posts = new ArrayList<>();
+    public static class Post {
+        private String title;
+        private String url;
+
+        public Post(String title, String url) {
+            this.title = title;
+            this.url = url;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        @Override
+        public String toString() {
+            return "Title: " + title + ", URL: " + url;
+        }
+    }
+    public List<Post> scrapeHackerNews() throws IOException {
+        List<Post> posts = new ArrayList<>();
         Document doc = Jsoup.connect("https://news.ycombinator.com/").get();
         Elements titles = doc.select(".titleline > a");
 
         for (Element title : titles) {
             String postTitle = title.text();
             String postUrl = title.attr("href");
-            posts.add(new Scrapes(postTitle, postUrl));
+            posts.add(new Post(postTitle, postUrl));
         }
         return posts;
     }
+
+//    public static void main(String[] args) {
+//        Scrape scraper = new Scrape();
+//        try {
+//            List<Post> posts = scraper.scrapeHackerNews();
+//            posts.forEach(System.out::println);
+//        } catch (IOException e) {
+//            System.err.println("Error scraping Hacker News: " + e.getMessage());
+//        }
+//    }
+
 }
